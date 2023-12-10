c9dceb1d33e70fd97c78dff3d58be80ec30ca325
Udayantha B.D.S <81279517+Dinuka-1999@users.noreply.github.com>
2023-12-10 21:35:07 +0530

    Update plotABR.m

--- ERP_practical/plotABR.m
+++ ERP_practical/plotABR.m
@@ -6,13 +6,13 @@ function plotABR(Data1,data2,Limit)
     
     for i=1:length(peaks)-1
         j = j + 1;
-        epochs(:,j) = data2(peaks(i)-50:peaks(i)+149);
+        epochs(:,j) = data2(peaks(i)-150:peaks(i)+299);
     end
 
     ensmbl_avg = mean(epochs(:,(1:length(peaks)-1)),2);
     %disp(length(ensmbl_avg));
     figure;
-    plot((-50:149)/10,ensmbl_avg*(10^6),'LineWidth',1)
+    plot((-150:299)/10,ensmbl_avg*(10^6),'LineWidth',1)
     title(['Ensemble averaged ABR from ',num2str(length(epochs)),' epochs'])
     ylim([-15 20]);
     ylabel('Amplitude(uV)');
